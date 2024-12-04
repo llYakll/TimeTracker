c092a619b342bb556ad0aa1aae7286611ed151f1
llYakll <zacharycsullivan@gmail.com>
2024-12-04 09:51:27 -0600

    redid my routes again, i realized some routes would be better handled elsewhere

--- backend/src/routes/job.js
+++ backend/src/routes/job.js
@@ -1,29 +1,26 @@
 import express from 'express';
-import { createJob,
-         getAllJobs,
-         getJobById,
-         updateJob,
-         deleteJobById, 
-         getEmployeesByJob, 
-         getJobsByEmployeeName, 
-         getJobsByCustomerName,
-         addEmployeeToJob, 
-         removeEmployeeFromJobByName
+import { 
+    createJob,
+    getAllJobs,
+    getJobById,
+    getJobsByStructureType,
+    getJobsByJobType,
+    getJobsByLocation,
+    updateJobById,
+    updateJobByLocation,
+    deleteJobById
         } from '../controllers/job.js';
 
 const jobRouter = express.Router(); // Explicitly named router so if used elsewhere it is clear what it is.
 
 //if z request is made to y endpoint it will execute x function.
-jobRouter.post('/jobs', createJob); 
-jobRouter.post('/jobs/:id/employees', addEmployeeToJob)
+jobRouter.post('/jobs', createJob);
 jobRouter.get('/jobs', getAllJobs);
 jobRouter.get('/jobs/:id', getJobById);
-jobRouter.get('/jobs/:id/employees', getEmployeesByJob)
-jobRouter.get('/jobs/employees/:firstName/:lastName', getJobsByEmployeeName)
-jobRouter.get('/jobs/:id/timelogs', getTimelogsByJob)
-jobRouter.get('/jobs/customers/:customerName', getJobsByCustomerName)
-jobRouter.put('/jobs/:id', updateJob); 
+jobRouter.get('/jobs/:structureType', getJobsByStructureType);
+jobRouter.get('/jobs/:jobType', getJobsByJobType);
+jobRouter.get('/jobs/:location', getJobsByLocation);
+jobRouter.put('/jobs/:id', updateJobById);
+jobRouter.put('/jobs/:location', updateJobByLocation);
 jobRouter.delete('/jobs/:id', deleteJobById);
-jobRouter.delete('/jobs/:id/employees/:firstName/:lastName', removeEmployeeFromJobByName)
-
 export default jobRouter; 
